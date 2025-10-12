8bbfe278878c1265a81f92673c8aa43d9a27ef24
Bernio <jendabernard@gmail.com>
2025-10-12 20:13:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_STATIC  = 'calc-static-v25';
-const CACHE_RUNTIME = 'calc-runtime-v25';
+const CACHE_STATIC  = 'calc-static-v26';
+const CACHE_RUNTIME = 'calc-runtime-v26';
 
 const PRECACHE = [
   '/',
@@ -63,6 +63,7 @@ self.addEventListener('fetch', (event) => {
   if (pathname.startsWith('/zpjevnicek/')) return; // nic nedělej
   // ... zbytek SW logiky Bernio ...
 });
+
 
 
 
