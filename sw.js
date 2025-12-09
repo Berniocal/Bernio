08275a9ee7e48e578ada28cc007c992672e157b8
Bernio <jendabernard@gmail.com>
2025-12-09 08:10:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // === Bernio SW – 1 listener, scope-safe cesty ===
-const CACHE_STATIC  = 'calc-static-v44';
-const CACHE_RUNTIME = 'calc-runtime-v44';
+const CACHE_STATIC  = 'calc-static-v45';
+const CACHE_RUNTIME = 'calc-runtime-v45';
 
 // Odvoď base-path ze scope (např. "/Bernio/")
 const BASE = new URL(self.registration.scope).pathname.replace(/\/+$/, '/') ;
@@ -89,6 +89,7 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+
 
 
 
