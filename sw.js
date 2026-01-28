5f6c11211a298a93d9fce7fdbc1235aec40b9a9f
Bernio <jendabernard@gmail.com>
2026-01-28 07:44:02 +0100

    Update service worker to version 64

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // === Bernio SW – 1 listener, scope-safe cesty ===
-const CACHE_STATIC  = 'calc-static-v63';
-const CACHE_RUNTIME = 'calc-runtime-v63';
+const CACHE_STATIC  = 'calc-static-v64';
+const CACHE_RUNTIME = 'calc-runtime-v64';
 
 // Odvoď base-path ze scope (např. "/Bernio/")
 const BASE = new URL(self.registration.scope).pathname.replace(/\/+$/, '/') ;
@@ -89,6 +89,7 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+
 
 
 
